86c9feb7e11b05f9ed7134cb3ba665672fecc472
Michał Januszewski <spock@gentoo.org>
2007-07-21 15:14:15 +0200

    testvbe: remove unused headers

--- testvbe.c
+++ testvbe.c
@@ -6,24 +6,16 @@
 #include <time.h>
 #include <fcntl.h>
 
-#include <sys/socket.h>
-#include <sys/poll.h>
-
-#include <linux/netlink.h>
-#include <linux/rtnetlink.h>
-
-#include <arpa/inet.h>
-
 #include "v86.h"
 
 int main(int argc, char *argv[])
 {
 	struct uvesafb_task tsk;
+	u8 *t;
 	u8 buf[4096];
 
 	if (v86_init())
 		return -1;
-
 	tsk.regs.eax = 0x4f00;
 	tsk.flags = TF_VBEIB;
 	tsk.buf_len = sizeof(struct vbe_ib);
@@ -34,14 +26,14 @@ int main(int argc, char *argv[])
 	printf("%s\n", ((struct vbe_ib*)buf)->oem_vendor_name_ptr + buf);
 
 	tsk.regs.eax = 0x4f02;
-	tsk.regs.ebx = 0x0118;
+	tsk.regs.ebx = 0xc161;
 	tsk.buf_len = 0;
 	tsk.flags = 0;
 
 	v86_task(&tsk, buf);
 
 	printf("got eax = %x\n", tsk.regs.eax);
-
+	
 	v86_cleanup();
 
 	return 0;
